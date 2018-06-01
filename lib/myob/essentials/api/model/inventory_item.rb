d0789ca69a2fe5b9e3aa114c3ec9b688b0cea80a
Vinh Tran <qvinh.tran@gmail.com>
2018-06-01 16:31:51 +1000

    new function for sale and item

--- lib/myob/essentials/api/model/inventory_item.rb
+++ lib/myob/essentials/api/model/inventory_item.rb
@@ -6,6 +6,11 @@ module Myob
           def model_route
             'businesses/:business_uid/inventory/items'
           end
+
+          def find_by_name_or_number(query)
+            object = { 'q' => query }
+            perform_request(url(object))
+          end
         end
       end
     end
--- lib/myob/essentials/api/model/sale_invoice.rb
+++ lib/myob/essentials/api/model/sale_invoice.rb
@@ -6,6 +6,11 @@ module Myob
           def model_route
             'businesses/:business_uid/sale/invoices'
           end
+
+          def find_by_invoice_number(invoice_number)
+            object = { 'invoiceNumber' => invoice_number }
+            perform_request(url(object))
+          end
         end
       end
     end
